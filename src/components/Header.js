c041fe67268e40cebfae3e5a297d99a0b2debffd
dagonib <davidgonzalezibanez@gmail.com>
2021-03-18 17:24:20 +0100

    Incluir SASS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Header.styles.scss'
+import React from 'react';
 import { Button } from 'react-bootstrap'
+import '../styles/header.styles.scss'
 
 const Header = () => {
     return (
-        <div>
-            Headers
-            <Button variant="info">Click me</Button>
+        <div className='header'>
+            <h1>Headers</h1>
+            <Button variant="danger">Click me</Button>
         </div>
     )
 }
