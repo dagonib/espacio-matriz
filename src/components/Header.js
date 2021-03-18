cad0470f04a4305ed378ecc7eb4816c1a86bc5f5
dagonib <davidgonzalezibanez@gmail.com>
2021-03-18 16:23:51 +0100

    include bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.styles.scss'
+import { Button } from 'react-bootstrap'
 
 const Header = () => {
     return (
         <div>
-            Header
+            Headers
+            <Button variant="info">Click me</Button>
         </div>
     )
 }
