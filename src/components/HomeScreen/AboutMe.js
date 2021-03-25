0e54d0def93f82be961a7c9878c50a5a528c3c48
dagonib <davidgonzalezibanez@gmail.com>
2021-03-25 12:50:23 +0100

    About me

---
+++ src/components/HomeScreen/AboutMe.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Container, Row, Col, Image, Button } from 'react-bootstrap'
+import TitleDeco from '../TitleDeco'
+
+import '../../styles/screens/homescreen/about-me.styles.scss'
+import imagen from '../../images/homescreen/imagen.jpg'
+
+const AboutMe = () => {
+    return (
+        <div className='bg-secondary m-0'>
+            <Container>
+                <Row className='d-flex flex-column flex-md-row py-5 m-0'>
+                    <Col className='aboutme-box-image d-flex align-items-end align-items-md-center p-0 ml-md-5 pb-lg-0 ml-lg-5' md={{ order: 'last'}}>                  
+                        <div className='d-flex flex-column w-100 w-md-50'>
+                            <p className='bg-dark text-white font-weight-bold text-uppercase p-3'>curiosidad</p>
+                            <p className='bg-dark text-white font-weight-bold text-uppercase p-3'>Pasión</p>
+                            <p className='bg-dark text-white font-weight-bold text-uppercase p-3'>Movimiento</p>
+                        </div>
+                    </Col>
+                    <Col className='d-flex flex-column align-items-lg-baseline justify-content-lg-end mt-5 mt-md-0 p-0' md={{ order: 'first'}}>
+                        <TitleDeco text={'Caroluna'}></TitleDeco>
+                        <h2 className="subtitle-text text-uppercase d-inline-block pb-4 mb-4">Soy mujer, soy lunar</h2>
+                        <p className="note-text text-justify m-0">
+                            Me despierto cada día buscando la curiosidad, interpretando mis propios deseos, identificando la belleza y gestando la mejor versión de mí misma. Soy mía y me comparto. Me reconozco auténtica para aceptarme en mis fases luminosas y oscuras, transitando la vida con consciencia.
+                            Los sesgos de género en la medicina y la escasa educación sexual, me habían impedido que tuviera plena autonomía sobre mi salud ginecológica y hormonal. Pero ya desperté. Cada día abro la ventana al conocimiento para saber más. Mi perspectiva aúnan el amor y la sana indignación para profundizar en mí y en mi misión de acompañar en el despertar.
+                        </p>
+                        <p className="note-text text-justify mt-3 m-0">
+                            ¡Mujeres por el bienestar del cuerpo, del bienestar común y de la Tierra!
+                        </p>    
+                        <Button variant='primary' type='submit' className='rounded-0 text-uppercase mt-3'>
+                            Conóceme
+                        </Button> 
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default AboutMe
--- src/components/HomeScreen/SubscribeForm.js
+++ src/components/HomeScreen/SubscribeForm.js
@@ -9,19 +9,19 @@ const SubscribeForm = () => {
             <Container>
                 <Form className='d-flex flex-column justify-content-center flex-md-row'>
                     <Col md={4} className='subscribe-main-box mt-0 mt-md-1 p-0'>
-                        <Form.Text className='subscribe-main-text text-white mr-3 m-0'>
+                        <Form.Text className='subscribe-main-text text-white mr-3 pt-0 pt-md-1 m-0'>
                             Recibe lo último vía email.
                         </Form.Text>
                     </Col>
                     <Col md={8} className='p-0'>
                         <Row className='d-flex flex-nowrap flex-column flex-md-row justify-content-left m-0'>
                             <Form.Group className='w-100 mr-0 mt-3 mr-md-3 mt-md-0 m-0'>
-                                <Form.Control type='text' placeholder='Tu nombre' name='nombre' className='subscribe-form-input'></Form.Control>
+                                <Form.Control type='text' placeholder='Tu nombre' name='nombre' className='subscribe-form-input rounded-0'></Form.Control>
                             </Form.Group>
                             <Form.Group  className='w-100 mr-0 mt-3 mr-md-3 mt-md-0 m-0'>
-                                <Form.Control type='email' placeholder='Tu email' name='nombre' className='subscribe-form-input'></Form.Control>
+                                <Form.Control type='email' placeholder='Tu email' name='nombre' className='subscribe-form-input rounded-0'></Form.Control>
                             </Form.Group>
-                            <Form.Group className='w-100 m-0 mt-3 mt-md-0'>
+                            <Form.Group className='w-100 m-0 mt-3 mt-md-0 rounded-0'>
                                 <Form.Control as='select' className='subscribe-form-input'>
                                     <option value=''>Elige una opción...</option>
                                     <option value='menstruación'>Menstruación</option>
@@ -29,12 +29,12 @@ const SubscribeForm = () => {
                                 </Form.Control>
                             </Form.Group>
                         </Row>
-                        <Row className='d-flex flex-column flex-md-row mt-3 m-0'>    
-                            <Button variant='primary' type='submit'>
+                        <Row className='d-flex flex-sm-row mt-3 m-0'>    
+                            <Button variant='primary' type='submit' className='rounded-0 text-uppercase mr-3'>
                                 Me apunto
                             </Button>         
-                            <Form.Group className='d-flex flex-row justify-content-center justify-content-md-left align-items-center mt-2 mt-md-0 ml-md-5 m-0'>
-                                <Form.Check className='p-0' type="checkbox" />
+                            <Form.Group className='d-flex flex-row justify-content-center justify-content-md-left align-items-center mt-2 mt-md-0 m-0'>
+                                <Form.Check className='' type="checkbox" />
                                 <Form.Text className='text-white m-0'>He leído y acepto la política de privacidad.</Form.Text>
                             </Form.Group>
                         </Row>
